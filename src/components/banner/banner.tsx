0397c1ed1a955bff4355c0765b71c412a79b21f2
juan_lorza <lorza112@hotmail.com>
2023-06-05 22:09:24 -0500

    feat: work juan

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -7,11 +7,17 @@ import SwiperCore, {
 } from "swiper";
 import styles from "@/styles/Banner.module.css";
 import Image from "next/image";
-import img1 from "./assets/20221120_164048.jpg";
-import img2 from "./assets/20221120_164145.jpg";
-import img3 from "./assets/20221120_164211.jpg";
-import img4 from "./assets/20221120_164047.jpg";
-import img6 from "./assets/20221120_164100.jpg";
+import img1 from "../../../public/assets/banner-img/1.png";
+import img2 from "../../../public/assets/banner-img/11.png";
+import img3 from "../../../public/assets/banner-img/12.png";
+import img4 from "../../../public/assets/banner-img/14.png";
+import img5 from "../../../public/assets/banner-img/18.png";
+import img6 from "../../../public/assets/banner-img/21.png";
+import img7 from "../../../public/assets/banner-img/2.png";
+import img8 from "../../../public/assets/banner-img/11.png";
+import img9 from "../../../public/assets/banner-img/5.png";
+import img10 from "../../../public/assets/banner-img/6.png";
+import img11 from "../../../public/assets/banner-img/1.png";
 import { BsFacebook, BsTwitter, BsInstagram } from "react-icons/bs";
 
 import "swiper/css";
@@ -26,96 +32,156 @@ function Banner() {
     <>
       <div className={styles.bannerContainer}>
         <div className={styles.bannerContainer__banner}>
-          <div className={styles.margen}>
-            <div className={styles.redes}>
-              <BsFacebook />
-              <BsTwitter />
-              <BsInstagram />
-            </div>
-            <div className={styles.vLine1}>{""}</div>
-            <br />
-            <p className={styles.p}>ETNIA</p>
-
-            <br />
-
-            <div className={styles.vLine3}></div>
-          </div>
-
-          <Swiper
-            autoplay={{
-              delay: 4000,
-              disableOnInteraction: false,
-            }}
-            slidesPerView={3}
-            spaceBetween={30}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[Pagination]}
-            className={styles.bannerContainer__banner__mySwiper}
-          >
-            <SwiperSlide
-              key={1}
-              className={styles.bannerContainer__banner__SwiperSlide}
+          <div className={styles.navBarContainer__banner}>
+            <Swiper
+              autoplay={{
+                delay: 4000,
+                disableOnInteraction: false,
+              }}
+              slidesPerView={3}
+              spaceBetween={30}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Pagination]}
+              className={styles.navBarContainer__banner__mySwiper}
             >
-              <Image
-                src={img1}
-                alt="img logo"
-                width={900}
-                height={550}
-                priority={true}
-              />
-            </SwiperSlide>
-            <SwiperSlide
-              key={2}
-              className={styles.bannerContainer__banner__SwiperSlide}
-            >
-              <Image
-                src={img2}
-                alt="img logo"
-                width={900}
-                height={550}
-                priority={true}
-              />
-            </SwiperSlide>
-            <SwiperSlide
-              key={3}
-              className={styles.bannerContainer__banner__SwiperSlide}
-            >
-              <Image
-                src={img3}
-                alt="img logo"
-                width={900}
-                height={550}
-                priority={true}
-              />
-            </SwiperSlide>
-            <SwiperSlide
-              key={4}
-              className={styles.bannerContainer__banner__SwiperSlide}
-            >
-              <Image
-                src={img4}
-                alt="img logo"
-                width={900}
-                height={550}
-                priority={true}
-              />
-            </SwiperSlide>
-            <SwiperSlide
-              key={6}
-              className={styles.bannerContainer__banner__SwiperSlide}
-            >
-              <Image
-                src={img6}
-                alt="img logo"
-                width={900}
-                height={550}
-                priority={true}
-              />
-            </SwiperSlide>
-          </Swiper>
-
+              <SwiperSlide
+                key={1}
+                className={styles.navBarContainer__banner__SwiperSlide}
+              >
+                <div className={styles.sssss}>
+                  <Image
+                    src={img1}
+                    alt="img"
+                    width={500}
+                    height={800}
+                    priority={true}
+                  />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide
+                key={2}
+                className={styles.navBarContainer__banner__SwiperSlide}
+              >
+                <Image
+                  src={img2}
+                  alt="img"
+                  width={500}
+                  height={800}
+                  priority={true}
+                />
+              </SwiperSlide>
+              <SwiperSlide
+                key={3}
+                className={styles.navBarContainer__banner__SwiperSlide}
+              >
+                <Image
+                  src={img3}
+                  alt="img"
+                  width={500}
+                  height={800}
+                  priority={true}
+                />
+              </SwiperSlide>
+              <SwiperSlide
+                key={4}
+                className={styles.navBarContainer__banner__SwiperSlide}
+              >
+                <Image
+                  src={img4}
+                  alt="img"
+                  width={500}
+                  height={800}
+                  priority={true}
+                />
+              </SwiperSlide>
+              <SwiperSlide
+                key={5}
+                className={styles.navBarContainer__banner__SwiperSlide}
+              >
+                <Image
+                  src={img5}
+                  alt="img"
+                  width={500}
+                  height={800}
+                  priority={true}
+                />
+              </SwiperSlide>
+              <SwiperSlide
+                key={6}
+                className={styles.navBarContainer__banner__SwiperSlide}
+              >
+                <Image
+                  src={img6}
+                  alt="img"
+                  width={500}
+                  height={800}
+                  priority={true}
+                />
+              </SwiperSlide>
+              <SwiperSlide
+                key={7}
+                className={styles.navBarContainer__banner__SwiperSlide}
+              >
+                <Image
+                  src={img7}
+                  alt="img"
+                  width={500}
+                  height={800}
+                  priority={true}
+                />
+              </SwiperSlide>
+              <SwiperSlide
+                key={8}
+                className={styles.navBarContainer__banner__SwiperSlide}
+              >
+                <Image
+                  src={img8}
+                  alt="img"
+                  width={500}
+                  height={800}
+                  priority={true}
+                />
+              </SwiperSlide>
+              <SwiperSlide
+                key={9}
+                className={styles.navBarContainer__banner__SwiperSlide}
+              >
+                <Image
+                  src={img9}
+                  alt="img"
+                  width={500}
+                  height={800}
+                  priority={true}
+                />
+              </SwiperSlide>
+              <SwiperSlide
+                key={10}
+                className={styles.navBarContainer__banner__SwiperSlide}
+              >
+                <Image
+                  src={img10}
+                  alt="img"
+                  width={500}
+                  height={800}
+                  priority={true}
+                />
+              </SwiperSlide>
+              <SwiperSlide
+                key={11}
+                className={styles.navBarContainer__banner__SwiperSlide}
+              >
+                <Image
+                  src={img11}
+                  alt="img"
+                  width={500}
+                  height={800}
+                  priority={true}
+                />
+              </SwiperSlide>
+            </Swiper>
+            {/*
           <Swiper
             autoplay={{
               delay: 3000,
@@ -132,8 +198,8 @@ function Banner() {
               <Image
                 src={img1}
                 alt="img logo"
-                width={900}
-                height={550}
+                width={500}
+                height={800}
                 priority={true}
               />
             </SwiperSlide>
@@ -144,8 +210,8 @@ function Banner() {
               <Image
                 src={img2}
                 alt="img logo"
-                width={900}
-                height={550}
+                width={500}
+                height={800}
                 priority={true}
               />
             </SwiperSlide>
@@ -156,8 +222,8 @@ function Banner() {
               <Image
                 src={img3}
                 alt="img logo"
-                width={900}
-                height={550}
+                width={500}
+                height={800}
                 priority={true}
               />
             </SwiperSlide>
@@ -168,8 +234,8 @@ function Banner() {
               <Image
                 src={img4}
                 alt="img logo"
-                width={900}
-                height={550}
+                width={500}
+                height={800}
                 priority={true}
               />
             </SwiperSlide>
@@ -180,17 +246,12 @@ function Banner() {
               <Image
                 src={img6}
                 alt="img logo"
-                width={900}
-                height={550}
+                width={500}
+                height={800}
                 priority={true}
               />
             </SwiperSlide>
-          </Swiper>
-          <div className={styles.vLines2}></div>
-          <div className={styles.redes2}>
-            <BsFacebook />
-            <BsTwitter />
-            <BsInstagram />
+          </Swiper> */}
           </div>
         </div>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,8 +15,8 @@ function Home() {
       </Head>
       <Banner />
       <Brands />
-      <KnowUs />
       <Products />
+      <KnowUs />
     </>
   );
 }
