9b76b710eeb76e31fe9e2dbd2c43ead6dea9ddbd
daniel-monsalve-villegas <danielmonsalve214164@correo.itm.edu.co>
2023-06-08 18:45:16 -0400

    filter working

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { products as prod } from "../../assets/dataProducts";
 import styles from "./Products.module.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export interface ProductModel {
   id: number;
@@ -27,14 +27,18 @@ const Products = () => {
     return (newElement = [...new Set(newElement)]);
   };
 
-  const brandType = getUniqueCategory(prod, "brand");
-
   const filterProducts = (products: ProductModel[]) => {
     return products.filter((product) => {
       return brand === "all" || product.brand === brand;
     });
   };
 
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newBrand = event.target.value;
+    setBrand(newBrand);
+  };
+
+  const brandType = getUniqueCategory(prod, "brand");
   const filteredProducts = filterProducts(prod);
 
   return (
@@ -45,36 +49,53 @@ const Products = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main className={styles.productsGlobalContainer}>
+        <h1 className="styles.productsContainer__title">PRODUCTOS</h1>
 
-      <h1 className="styles.productsContainer">PRODUCTOS</h1>
-
-      <div className={styles.productsContainer}>
-        <h3>Marcas</h3>
-        <ul>
-          {brandType.map((brand, idx) => (
-            <li key={idx}>
+        <div className={styles.productsContainer}>
+          <h3>Marcas</h3>
+          <ul>
+            <li>
               <div>
-                <input type="checkbox" name="brand" value={brand} />
-                <span>{brand}</span>
+                <input
+                  type="radio"
+                  name="brand"
+                  value="all"
+                  onChange={handleCheckboxChange}
+                />
+                <span>Todas</span>
               </div>
             </li>
-          ))}
-        </ul>
-      </div>
+            {brandType.map((brand, idx) => (
+              <li key={idx}>
+                <div>
+                  <input
+                    type="radio"
+                    name="brand"
+                    value={brand}
+                    onChange={handleCheckboxChange}
+                  />
+                  <span>{brand}</span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-      <div className={styles.products__div}>
-        {filteredProducts.map((product) => (
-          <Link key={product.id} href={`/products/${product.id}`}>
-            <h2>{product.name}</h2>
-            <Image
-              src={product.image}
-              alt={""}
-              width={200}
-              height={250}
-            ></Image>
-          </Link>
-        ))}
-      </div>
+        <div className={styles.products__div}>
+          {filteredProducts.map((product) => (
+            <Link key={product.id} href={`/products/${product.id}`}>
+              <h2>{product.name}</h2>
+              <Image
+                src={product.image}
+                alt={""}
+                width={200}
+                height={250}
+              ></Image>
+            </Link>
+          ))}
+        </div>
+      </main>
     </>
   );
 };
